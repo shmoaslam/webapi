bf98cd0084a7c950f341595c00f3dbdf28088d7f
shmoaslam <shmoaslam@gmail.com>
2015-09-11 15:38:25 +0530

    uploading ioc

---
+++ WebAPI/Core/IOC/UnityConfig.cs
@@ -0,0 +1,16 @@
+﻿using BusinessServices;
+using Microsoft.Practices.Unity;
+
+namespace Core.IOC
+{
+    public static class UnityConfig
+    {
+        public static UnityContainer RegisterType()
+        {
+            var container = new UnityContainer();
+            container.RegisterType<IProductServices, ProductServices>(new HierarchicalLifetimeManager());
+
+            return container;
+        }
+    }
+}
---
+++ WebAPI/WebAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using Core.IOC;
+using System.Web.Http;
+
+namespace WebAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            config.DependencyResolver = new UnityResolver(UnityConfig.RegisterType());
+
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
